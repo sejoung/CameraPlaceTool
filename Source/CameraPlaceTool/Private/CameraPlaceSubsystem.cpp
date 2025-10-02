37f342ec051c48528fc9cf4d461ba3cf43e98b82
sejoung kim <sejoung@gmail.com>
2025-10-02 11:50:10 +0900

    ㄴㅁㅇ

--- Source/CameraPlaceTool/Private/CameraPlaceSubsystem.cpp
+++ Source/CameraPlaceTool/Private/CameraPlaceSubsystem.cpp
@@ -1,7 +1,6 @@
 #include "CameraPlaceSubsystem.h"
 #include "CameraPlaceSettings.h"
 #if WITH_EDITOR
-#include "Subsystems/EditorSubsystem.h"
 #include "Editor.h"
 #include "EditorViewportClient.h"
 #include "Kismet/GameplayStatics.h"
