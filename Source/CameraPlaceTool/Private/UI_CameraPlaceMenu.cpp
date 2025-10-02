e27abf98aabfe5849b72e1c890d7dd200df2e3b9
sejoung kim <sejoung@gmail.com>
2025-10-02 13:00:15 +0900

    추가

--- Source/CameraPlaceTool/Private/UI_CameraPlaceMenu.cpp
+++ Source/CameraPlaceTool/Private/UI_CameraPlaceMenu.cpp
@@ -1,53 +1,70 @@
+#include "UI_CameraPlaceMenu.h"
 #include "CameraPlaceSubsystem.h"
 #include "ToolMenus.h"
 #include "LevelEditor.h"
 #include "Framework/Commands/InputChord.h"
 #include "Framework/Commands/UICommandList.h"
 #include "Framework/Commands/Commands.h"
-#define LOCTEXT_NAMESPACE "CameraPlaceTool"   
+#include "Styling/AppStyle.h"
+
+#define LOCTEXT_NAMESPACE "CameraPlaceTool"
+
 class FCameraPlaceCommands : public TCommands<FCameraPlaceCommands>
 {
 public:
     FCameraPlaceCommands()
-    : TCommands<FCameraPlaceCommands>(TEXT("CameraPlaceTool"), NSLOCTEXT("CameraPlace", "CameraPlace", "CameraPlaceTool"), NAME_None, FAppStyle::GetAppStyleSetName()) {}
-
+    : TCommands<FCameraPlaceCommands>(TEXT("CameraPlaceTool"),
+      LOCTEXT("CameraPlace", "CameraPlaceTool"),
+      NAME_None, FAppStyle::GetAppStyleSetName()) {}
     virtual void RegisterCommands() override
     {
-        UI_COMMAND(PlaceFromCamera, "Place From Camera", "Place the selected asset from camera forward", EUserInterfaceActionType::Button, FInputChord(EModifierKey::Control | EModifierKey::Alt, EKeys::P));
+        UI_COMMAND(PlaceFromCamera, "Place From Camera",
+            "Place the selected asset from camera forward",
+            EUserInterfaceActionType::Button,
+            FInputChord(EModifierKey::Control | EModifierKey::Alt, EKeys::P));
     }
-
     TSharedPtr<FUICommandInfo> PlaceFromCamera;
 };
 
-static TSharedPtr<FUICommandList> CommandList;
+static TSharedPtr<FUICommandList> GCmdList;
 
 static void ExecutePlaceFromCamera()
 {
     if (GEditor)
     {
-        if (UCameraPlaceSubsystem* Sys = GEditor->GetEditorSubsystem<UCameraPlaceSubsystem>())
-        {
+        if (auto* Sys = GEditor->GetEditorSubsystem<UCameraPlaceSubsystem>())
             Sys->PlaceSelectedFromCamera();
-        }
     }
 }
 
-static void RegisterMenus()
+void RegisterCameraPlaceMenus()
 {
-    UToolMenus* Menus = UToolMenus::Get();
+    FCameraPlaceCommands::Register();
+    GCmdList = MakeShared<FUICommandList>();
+    GCmdList->MapAction(FCameraPlaceCommands::Get().PlaceFromCamera,
+        FExecuteAction::CreateStatic(&ExecutePlaceFromCamera));
 
-    // 레벨 에디터 툴바 버튼
-    if (UToolMenu* Toolbar = Menus->ExtendMenu("LevelEditor.LevelEditorToolBar"))
+    UToolMenus::RegisterStartupCallback(FSimpleMulticastDelegate::FDelegate::CreateLambda([]()
     {
-        FToolMenuSection& Section = Toolbar->AddSection("CameraPlace", NSLOCTEXT("CameraPlace", "Section", "CameraPlace"));
-        Section.AddEntry(FToolMenuEntry::InitToolBarButton(FCameraPlaceCommands::Get().PlaceFromCamera));
-    }
-
-    // Content Browser 우클릭 메뉴
-    if (UToolMenu* CBMenu = Menus->ExtendMenu("ContentBrowser.AssetContextMenu"))
-    {
-        FToolMenuSection& Section = CBMenu->AddSection("CameraPlaceCtx", NSLOCTEXT("CameraPlace", "Ctx", "CameraPlaceTool"));
-        Section.AddMenuEntry(FCameraPlaceCommands::Get().PlaceFromCamera);
-    }
+        if (UToolMenu* Toolbar = UToolMenus::Get()->ExtendMenu("LevelEditor.LevelEditorToolBar"))
+        {
+            FToolMenuSection& Section = Toolbar->AddSection("CameraPlace", LOCTEXT("Section","CameraPlace"));
+            Section.AddEntry(FToolMenuEntry::InitToolBarButton(FCameraPlaceCommands::Get().PlaceFromCamera));
+        }
+        if (UToolMenu* CB = UToolMenus::Get()->ExtendMenu("ContentBrowser.AssetContextMenu"))
+        {
+            FToolMenuSection& Section = CB->AddSection("CameraPlaceCtx", LOCTEXT("Ctx","CameraPlaceTool"));
+            Section.AddMenuEntry(FCameraPlaceCommands::Get().PlaceFromCamera);
+        }
+    }));
 }
+
+void UnregisterCameraPlaceMenus()
+{
+    UToolMenus::UnRegisterStartupCallback(nullptr);
+    UToolMenus::UnregisterOwner(nullptr);
+    GCmdList.Reset();
+    FCameraPlaceCommands::Unregister();
+}
+
 #undef LOCTEXT_NAMESPACE
