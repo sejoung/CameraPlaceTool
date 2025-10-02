86ab0d975c747b8547bb5e4240d49f8c99ae013f
sejoung kim <sejoung@gmail.com>
2025-10-02 15:44:13 +0900

    123

--- Source/CameraPlaceTool/Public/CameraPlaceSettings.h
+++ Source/CameraPlaceTool/Public/CameraPlaceSettings.h
@@ -8,9 +8,9 @@ class UCameraPlaceSettings : public UObject
 {
     GENERATED_BODY()
 public:
-    UPROPERTY(EditAnywhere, Config, Category="CameraPlace") float TraceDistance = 100.f; // 1km
-    UPROPERTY(EditAnywhere, Config, Category="CameraPlace") float FallbackDistance = 2000.f; // 미히트 시
-    UPROPERTY(EditAnywhere, Config, Category="CameraPlace") float SurfaceOffset = 0.5f; // cm
+    UPROPERTY(EditAnywhere, Config, Category="CameraPlace") float TraceDistance = 100.f; 
+    UPROPERTY(EditAnywhere, Config, Category="CameraPlace") float FallbackDistance = 200.f; // 미히트 시
+    UPROPERTY(EditAnywhere, Config, Category="CameraPlace") float SurfaceOffset = 0f; // cm
     UPROPERTY(EditAnywhere, Config, Category="CameraPlace") bool bAlignToSurface = true; // 표면 노멀에 업 정렬
     UPROPERTY(EditAnywhere, Config, Category="CameraPlace") bool bKeepWorldUp = false; // 월드 업 유지(체크 시 bAlignToSurface 무시)
 };
