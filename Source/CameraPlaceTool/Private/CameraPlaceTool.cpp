9108d3b96209d027b5ddb9df36259b96159bfbae
sejoung kim <sejoung@gmail.com>
2025-10-02 13:19:43 +0900

    11

--- Source/CameraPlaceTool/Private/CameraPlaceTool.cpp
+++ Source/CameraPlaceTool/Private/CameraPlaceTool.cpp
@@ -1,20 +1,18 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "CameraPlaceTool.h"
+#include "UI_CameraPlaceMenu.h"
 
 #define LOCTEXT_NAMESPACE "FCameraPlaceToolModule"
 
 void FCameraPlaceToolModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+		RegisterCameraPlaceMenus();
 }
 
 void FCameraPlaceToolModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+	UnregisterCameraPlaceMenus();
 }
 
 #undef LOCTEXT_NAMESPACE
-	
-IMPLEMENT_MODULE(FCameraPlaceToolModule, CameraPlaceTool)
